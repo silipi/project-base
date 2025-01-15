e7e7f3743c8646993dc929c87a8ba8e2e8f77495
Gabriel Silipi <gabriel.silipi@gmail.com>
2025-01-15 13:26:08 -0300

    chore: :fire: removing Modal component hooks

--- webapp/src/pages/Home/index.tsx
+++ webapp/src/pages/Home/index.tsx
@@ -1,17 +1,12 @@
 import { Button } from '@/components/shadcn/ui/button';
 import useLogout from '@/hooks/useLogout';
-import useModal from '@/hooks/useModal';
 
 const Home = () => {
   const { logout } = useLogout();
-  const { openModal } = useModal();
 
   return (
     <div>
       <Button onClick={logout}>Logout</Button>
-      <Button onClick={() => openModal('test', <div>teste</div>)}>
-        Open Modal
-      </Button>
     </div>
   );
 };
